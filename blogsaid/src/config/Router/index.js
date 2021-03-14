3afe6744e44c09c48c1483fd1d01d7145a85f744
jejaksaid <said@fastlearningcenter.com>
2021-03-14 19:27:54 +0700

    blogsaid

---
+++ blogsaid/src/config/Router/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+	return;
+	<div>
+		<Router>
+			<Switch>
+				<Route></Route>
+			</Switch>
+		</Router>
+	</div>;
+};
+
+export default Routes;
