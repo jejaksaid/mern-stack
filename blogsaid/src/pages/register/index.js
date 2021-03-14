e29813dd569968a12cd109dfc961ac32c634b11e
jejaksaid <said@fastlearningcenter.com>
2021-03-15 00:39:33 +0700

    blogsaid recreating

--- blogsaid/src/pages/register/index.js
+++ blogsaid/src/pages/register/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./register.scss";
+import { RegisterBg } from "../../assets";
+import { Input, Button } from "../../components/atoms";
 
 const Register = () => {
 	return (
 		<div className="main-page">
 			<div className="left">
-				<p>picture</p>
+				<img src={RegisterBg} className="bg-image" alt="imageBg" />
 			</div>
 			<div className="right">
 				<p>Register</p>
+				<Input label="Full Name" placeholder="Full Name" />
+				<Input label="Email" placeholder="Email" />
+				<Input label="Password" placeholder="Password" />
+				<Button title="Register" />
 			</div>
 		</div>
 	);
