45e49d6d26066e20cc5c19578ea86ea999ae8a20
jejaksaid <said@fastlearningcenter.com>
2021-03-17 23:21:10 +0700

    components

--- blogsaid/src/components/atoms/index.js
+++ blogsaid/src/components/atoms/index.js
@@ -1,4 +1,6 @@
 import Input from "./input";
 import Button from "./button";
+import Gap from "./gap";
+import Link from "./link";
 
-export { Input, Button };
+export { Input, Button, Gap, Link };
--- blogsaid/src/pages/register/index.js
+++ blogsaid/src/pages/register/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./register.scss";
 import { RegisterBg } from "../../assets";
-import { Input, Button } from "../../components/atoms";
+import { Input, Button, Gap, Link } from "../../components";
 
 const Register = () => {
 	return (
@@ -9,12 +9,18 @@ const Register = () => {
 			<div className="left">
 				<img src={RegisterBg} className="bg-image" alt="imageBg" />
 			</div>
+
 			<div className="right">
-				<p className="title">Register</p>
+				<p className="title ">Register</p>
 				<Input label="Full Name" placeholder="Full Name" />
+				<Gap height={18} />
 				<Input label="Email" placeholder="Email" />
+				<Gap height={18} />
 				<Input label="Password" placeholder="Password" />
+				<Gap height={50} />
 				<Button title="Register" />
+				<Gap height={100} />
+				<Link title="Kembali ke Login" />
 			</div>
 		</div>
 	);
