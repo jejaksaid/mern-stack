2180a02f5206bcaa26484e6cb9007d6fa208ebb7
jejaksaid <said@fastlearningcenter.com>
2021-03-20 13:55:53 +0700

    styling MainApp

--- blogsaid/src/assets/index.js
+++ blogsaid/src/assets/index.js
@@ -1,4 +1,11 @@
 import RegisterBg from "./images/register-bg.jpg";
 import LoginBg from "./images/login-bg.jpg";
+import Logo from "./images/logo.svg";
 
-export { RegisterBg, LoginBg };
+// icons
+
+import ICYoutube from "./icons/youtube.svg";
+import ICTwitter from "./icons/twitter.svg";
+import ICGithub from "./icons/github.svg";
+
+export { RegisterBg, LoginBg, Logo, ICGithub, ICTwitter, ICYoutube };
