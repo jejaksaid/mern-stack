fafc043654c262839a240afea1c64c6dc4caef26
jejaksaid <said@fastlearningcenter.com>
2021-03-21 23:44:03 +0700

    styling blog post, pagination

--- blogsaid/src/pages/home/index.js
+++ blogsaid/src/pages/home/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button } from "../../components";
+import "./home.scss";
 
 const Home = () => {
 	return (
-		<div>
+		<div className="home-page-wrapper">
 			<div className="create-wrapper">
 				<Button title="create blog post" />
 			</div>
