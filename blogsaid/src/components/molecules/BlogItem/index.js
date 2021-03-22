0d2271a036f9e3db82684d75a88a2dd5c0cfa5d6
jejaksaid <said@fastlearningcenter.com>
2021-03-22 19:50:18 +0700

    styling blog post and pagination

--- blogsaid/src/components/molecules/BlogItem/index.js
+++ blogsaid/src/components/molecules/BlogItem/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { RegisterBg } from "../../../assets";
 
 const BlogItem = () => {
 	return (
-		<div>
-			<p>Content blog item</p>
+		<div className="blog-item">
+			<img className="image-thumb" src={RegisterBg} alt="post" />
+			<div className="content-detail">
+				<p className="title">Title Blog</p>
+				<p className="author">Author - Date post</p>
+				<p className="body">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+					iste hic autem maxime excepturi natus accusantium soluta pariatur
+					magni officiis quae esse cupiditate, corporis dolore est ratione,
+					laborum repellat. Ipsa.
+				</p>
+			</div>
 		</div>
 	);
 };
