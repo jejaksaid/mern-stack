0bf5ecfaad4fe0eac4e29339f9c48aec82d9907b
jejaksaid <said@fastlearningcenter.com>
2021-03-23 19:28:38 +0700

    styling create blog post

--- blogsaid/src/pages/CreateBlog/index.js
+++ blogsaid/src/pages/CreateBlog/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, Input } from "../../components";
 
 const CreateBlog = () => {
-	return <div>Content Create Blog Page</div>;
+	return (
+		<div>
+			<p>Create New Blog Post</p>
+			<Input label="Post Title" />
+			<p>Upload Image</p>
+			<textarea></textarea>
+			<Button title="Save" />
+		</div>
+	);
 };
 
 export default CreateBlog;
