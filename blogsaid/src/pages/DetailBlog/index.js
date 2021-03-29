bedc48c9fbc952bdd6c9a757105367cedadc332d
jejaksaid <said@fastlearningcenter.com>
2021-03-29 20:14:41 +0700

    styling detail blog post

--- blogsaid/src/pages/DetailBlog/index.js
+++ blogsaid/src/pages/DetailBlog/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
 
 const DetailBlog = () => {
-	return <div>Content Detail Blog Page</div>;
+	return (
+		<div>
+			<img src={RegisterBg} alt="thumb" />
+			<p>Title Blog</p>
+			<p>Author - Date Post</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+				dolore veritatis sequi labore dolores vel quis consequatur soluta ipsa,
+				eaque voluptatibus id, necessitatibus fugit minima accusamus possimus
+				nisi iste reprehenderit!
+			</p>
+		</div>
+	);
 };
 
 export default DetailBlog;
