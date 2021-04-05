357388b36f5ba1d1b9574b945a41c57ef6da535d
jejaksaid <said@fastlearningcenter.com>
2021-04-05 23:41:34 +0700

    restructure project nodejs/expressjs

--- blog-api/index.js
+++ blog-api/index.js
@@ -1,27 +1,8 @@
 const express = require("express");
 
 const app = express();
-const router = express.Router();
+const productRoutes = require("./src/routes/products");
 
-router.use("/products", (req, res, next) => {
-	res.json({ name: "saidramadhan", email: "saidramadhan3@gmail.com" });
-	next();
-});
-
-router.use("/price", (req, res, next) => {
-	res.json({ price: 10000000 });
-	next();
-});
-router.get("/customers", (req, res, next) => {
-	res.json({ title: "customer 1" });
-	next();
-});
-
-router.delete("/users", (req, res, next) => {
-	res.json({ user: "user 1" });
-});
-app.use("/", router);
-
-// GET '/users' ==> [{name: said}]
+app.use("/", productRoutes);
 
 app.listen(4000);
