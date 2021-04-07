c21db5f3019ba8da96ee5b62e301d261f171e7dd
jejaksaid <said@fastlearningcenter.com>
2021-04-08 06:21:25 +0700

    error cors origin, API nodejs

---
+++ blog-api/codepen/main.js
@@ -0,0 +1,10 @@
+const getButton = document.getElementById("get");
+const postButton = document.getElementById("post");
+
+getButton.addEventListener("click", () => {
+	alert("halo");
+});
+
+postButton.addEventListener("click", () => {
+	alert("hallo again");
+});
--- blog-api/src/routes/products.js
+++ blog-api/src/routes/products.js
@@ -7,7 +7,7 @@ const productsController = require("../controllers/products");
 // CREATE -> POST : localhost:4000/v1/customer/product
 router.post("/product", productsController.createProduct);
 
-// READ -> GET
+// READ -> GET : localhost:4000/v1/customer/products
 router.get("/products", productsController.getAllProducts);
 
 module.exports = router;
