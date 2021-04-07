1d19d896ae125e8384df989a42d22359610e6c06
jejaksaid <said@fastlearningcenter.com>
2021-04-08 06:39:14 +0700

    cors origin, API nodejs

--- blog-api/codepen/main.js
+++ blog-api/codepen/main.js
@@ -1,10 +1,26 @@
 const getButton = document.getElementById("get");
 const postButton = document.getElementById("post");
 
+// getButton.addEventListener("click", () => {
+// 	alert("halo");
+// });
+
+// postButton.addEventListener("click", () => {
+// 	alert("hallo again");
+// });
+
 getButton.addEventListener("click", () => {
-	alert("halo");
+	fetch("http://localhost:4000/v1/customer/products")
+		.then((res) => res.json())
+		.then((resJSON) => console.log(resJSON))
+		.catch((err) => console.log(err));
 });
 
 postButton.addEventListener("click", () => {
-	alert("hallo again");
+	fetch("http://localhost:4000/v1/customer/product", {
+		method: "POST",
+	})
+		.then((res) => res.json())
+		.then((resJSON) => console.log(resJSON))
+		.catch((err) => console.log(err));
 });
