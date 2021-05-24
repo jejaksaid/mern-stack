0146247c729838d7d4d7308d32447645d059f5fc
jejaksaid <said@fastlearningcenter.com>
2021-05-24 20:56:48 +0700

    delete method

--- blog-api/src/controllers/blog.js
+++ blog-api/src/controllers/blog.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 const BlogPost = require("../models/blog");
+const path = require("path");
+const fs = require("fs");
 
 exports.createBlogPost = (req, res, next) => {
 	const errors = validationResult(req);
@@ -118,4 +120,37 @@ exports.updateBlogPost = (req, res, next) => {
 		.catch((err) => {
 			next(err);
 		});
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+	const postId = req.params.postId;
+
+	BlogPost.findById(postId)
+		.then((post) => {
+			if (!post) {
+				const error = new Error("Blog Post Tidak Ditemukan");
+				error.errorStatus = 404;
+				throw error;
+			}
+
+			removeImage(post.image);
+			return BlogPost.findByIdAndRemove(postId);
+		})
+		.then((result) => {
+			res.status(200).json({
+				message: "Hapus Blog Post Berhasil",
+				data: result,
+			});
+		})
+		.catch((err) => {
+			next(err);
+		});
+};
+
+const removeImage = (filePath) => {
+	console.log("filePath", filePath);
+	console.log("dir name: ", __dirname);
+
+	filePath = path.join(__dirname, "../..", filePath);
+	fs.unlink(filePath, (err) => console.log(err));
 };
